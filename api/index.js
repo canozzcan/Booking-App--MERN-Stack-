cf5ef85c6d1a946f55e36385b2842671e279dd66
canozzcan <canozcanstu@gmail.com>
2022-07-30 19:30:03 +0300

    Hotel CRUD

--- api/index.js
+++ api/index.js
@@ -24,10 +24,13 @@ mongoose.connection.on("disconnected", () => {
 })
 
 // middlewares
+app.use(express.json())
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
 
 
 app.listen(8800, () => {
